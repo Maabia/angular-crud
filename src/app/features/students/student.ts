c30f6552c7599e71c641340ac1b8ebf108a6ee9b
Maabia <maabia.ferry16@gmail.com>
2021-10-01 20:56:41 -0300

    classe student configurada

--- src/app/features/students/student.ts
+++ src/app/features/students/student.ts
@@ -1,2 +1,14 @@
 export class Student {
+  id: number;
+  name: string;
+  email: string;
+  birthday: string;
+
+  constructor(id?: number, name?: string, email?: string, birthday?: string ) {
+    this.id = id || 0; // se o id estiver nulo, eu qro que o default seja zero.
+    // se for vazio, se for undefined, se eu não informar eu qro que seja uma string vazia. E será assim com todos os demais.
+    this.name = name || '';
+    this.email = email || '';
+    this.birthday = birthday || '';
+  }
 }
