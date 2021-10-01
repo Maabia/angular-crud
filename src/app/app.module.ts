86c307913ca624b51f06edc9f0c6657f83636aaf
Maabia <maabia.ferry16@gmail.com>
2021-10-01 20:39:13 -0300

    Students-list alterado para students module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { StudentListComponent } from './features/students/student-list/student-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/features/students/students.module.ts
+++ src/app/features/students/students.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentEditComponent } from './student-edit/student-edit.component';
 import { StudentNewComponent } from './student-new/student-new.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 
 @NgModule({
   declarations: [
     StudentEditComponent,
-    StudentNewComponent
+    StudentNewComponent,
+    StudentListComponent
   ],
   imports: [
     CommonModule,
