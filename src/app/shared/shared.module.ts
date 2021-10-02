f15ddcb9dffd546731e1a20b69d99c4d871ac49f
Maabia <maabia.ferry16@gmail.com>
2021-10-02 12:37:56 -0300

    criado arquivo de pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgePipe } from './pipes/age.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgePipe
+  ],
   imports: [
     CommonModule
   ]
