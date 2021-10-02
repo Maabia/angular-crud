c6bc2f69245f13c3798cfa66dc177bee929a0e9b
Maabia <maabia.ferry16@gmail.com>
2021-10-02 12:15:45 -0300

    html e component implementado

--- src/app/features/students/student-new/student-new.component.ts
+++ src/app/features/students/student-new/student-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Student } from '../student';
+import { StudentsService } from '../students.service';
 
 @Component({
   selector: 'app-student-new',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentNewComponent implements OnInit {
 
-  constructor() { }
+  student: Student = new Student();
+
+  constructor(
+    private router: Router,
+    private studentService: StudentsService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.studentService.save(this.student);
+    this.router.navigateByUrl("students");
   }
 
 }
