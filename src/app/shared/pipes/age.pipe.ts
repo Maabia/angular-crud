548f54454a338814d9b6929c712304198fe3db37
Maabia <maabia.ferry16@gmail.com>
2021-10-02 12:47:38 -0300

    pipe criado

--- src/app/shared/pipes/age.pipe.ts
+++ src/app/shared/pipes/age.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AgePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): number {
+    const dateMilliseconds = this.formatStringDate(value);
+    const diffMilliseconds = Date.now() - dateMilliseconds;
+
+    return Math.abs(new Date(diffMilliseconds).getUTCFullYear() - 1970);
+  }
+
+  private formatStringDate(stringDate: string): number {
+    const [year, month, day] = stringDate.split('-');
+    return Date.UTC(parseInt(year), parseInt(month) -1, parseInt(day));
   }
 
 }
