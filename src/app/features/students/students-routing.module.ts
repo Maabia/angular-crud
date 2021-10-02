e5932d4334f5b98144c10fe7ab0792a58ff77c33
Maabia <maabia.ferry16@gmail.com>
2021-10-01 21:52:55 -0300

    rotas definidas

--- src/app/features/students/students-routing.module.ts
+++ src/app/features/students/students-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentNewComponent } from './student-new/student-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StudentListComponent
+  },
+  {
+    path: 'new',
+    component: StudentNewComponent
+  },
+  {
+    path: ':id',
+    component: StudentEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
